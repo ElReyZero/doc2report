bb5c58407329197ce10815d1f3382062ab18e90c
Juan Romero <juan.romero1201@gmail.com>
2022-10-09 22:02:33 -0500

    add uuid to document model

--- reports/models.py
+++ reports/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 class Document(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, blank=True)
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
